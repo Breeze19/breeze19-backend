4cca02273c38d4eeb8cbf8b26405f28265812d7c
sd1998 <shashvat51@gmail.com>
2018-11-27 19:47:34 +0530

    Changes

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const firebase = require('firebase');
+const config = require('./config.js');
+const app = express();
+
+const PORT = 8080 || process.env.PORT;
+
+firebase.initializeApp(config)
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/register',function(req,res){
+  const data = req.body;
+  firebase.database().ref('/data/registrations').push(data)
+  res.json({
+    result: "OK"
+  })
+})
+
+app.get('/email',function(req,res){
+  const data = req.body;
+  firebase.database().ref('/data/email').push(data)
+  res.json({
+    result: "OK"
+  })
+})
+
+app.listen(PORT,function(){
+  console.log("Listening to " + PORT)
+})
