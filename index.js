a3024f4859dab7791bfc81f0b45d0744616d3188
sd1998 <shashvat51@gmail.com>
2018-11-27 20:02:22 +0530

    Bug fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const firebase = require('firebase');
 const config = require('./config.js');
 const app = express();
 
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080
 
 firebase.initializeApp(config)
 
