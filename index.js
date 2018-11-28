0d8f6af4b6dcde86e6db64486223fb26fa5c21e7
sd1998 <shashvat51@gmail.com>
2018-11-28 17:09:43 +0530

    Callbacks added

--- index.js
+++ index.js
@@ -15,17 +15,29 @@ app.use(cors())
 
 app.post('/register',function(req,res){
   const data = req.body.data;
-  firebase.database().ref('/data/registrations').push(data)
-  res.json({
-    result: "OK"
+  firebase.database().ref('/data/registrations').push(data).then(function(){
+    res.json({
+      result: "OK"
+    })
+  }).catch(function(err){
+    console.log(err);
+    res.json({
+      result: "Error"
+    })
   })
 })
 
 app.post('/email',function(req,res){
   const data = req.body.data;
-  firebase.database().ref('/data/email').push(data)
-  res.json({
-    result: "OK"
+  firebase.database().ref('/data/email').push(data).then(function(){
+    res.json({
+      result: "OK"
+    })
+  }).catch(function(err){
+    console.log(err)
+    res.json({
+      result: "Error"
+    })
   })
 })
 
