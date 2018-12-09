43c9abb59f979d704330e8d03fb33137b3b46b8e
sd1998 <shashvat51@gmail.com>
2018-12-09 14:25:52 +0530

    Registrations endpoint added

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const firebase = require('firebase');
 const config = require('./config.js');
+const createCSVWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
 const app = express();
 
 const PORT = process.env.PORT || 8080
 
-firebase.initializeApp(config)
+firebase.initializeApp(config.FIREBASE_CONFIG)
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
@@ -67,6 +69,57 @@ app.post('/email',function(req,res){
       result: "Error"
     })
   })
+})
+
+app.get('/getcsv/:key',function(req,res){
+  if(req.params.key == config.SECURE_KEY){
+    const csvWriter = createCSVWriter({
+      path: './registrations.csv',
+      header: [
+        {id: 'cgg',title: 'CGPA grad'},
+        {id: 'cgx',title: 'CGPA 10th'},
+        {id: 'cgxii',title: 'CGPA 12th'},
+        {id: 'collegeName',title: 'College'},
+        {id: 'email',title: 'Email'},
+        {id: 'lod',title: 'Location of Domicile'},
+        {id: 'name',title: 'Name'},
+        {id: 'noofplayers',title: 'No of. Players'},
+        {id: 'phno',title: 'Phone no.'},
+        {id: 'sportsName',title: 'Sports Name'},
+        {id: 'stream',title: 'Stream'}
+      ]
+    })
+    firebase.database().ref('/data/registrations').once('value').then(function(snapshot){
+      if(snapshot.val()){
+        try{
+          if(fs.existsSync('./registrations.csv')){
+            console.log('File exists')
+            fs.unlinkSync('./registrations.csv')
+          }
+        } catch(err){
+          console.log(err)
+        }
+        registrations = []
+        snapshot.forEach(function(child){
+          registrations.push(child.val())
+          csvWriter.writeRecords(registrations).then(function(){
+            console.log("File created");
+            var stat = fs.statSync('./registrations.csv')
+            res.writeHead(200,{
+              'Content-Type': 'text/csv',
+              'Content-Length': stat.size
+            })
+            var readStream = fs.createReadStream('./registrations.csv')
+            readStream.pipe(res)
+          })
+        })
+      }
+    })
+  } else{
+    res.json({
+      result: "Unauthorized attempt"
+    })
+  }
 })
 
 app.listen(PORT,function(){
