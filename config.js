585f6bc4857c240a1e26e4f88c7d97323222df82
sd1998 <shashvat51@gmail.com>
2019-01-08 19:43:59 +0530

    nodemailer config changes

--- config.js
+++
@@ -1,7 +0,0 @@
-const credentials = {
-  SECURE_KEY: "545A949BB77A1527E5A19DE5C44BBA19738D0450CD5DC7683DC5CD33BAA0F7D9",
-  GMAIL_EMAIL_ID: "breeze.events@snu.edu.in",
-  GMAIL_PASSWORD: "dontdie2019"
-}
-
-module.exports = credentials
